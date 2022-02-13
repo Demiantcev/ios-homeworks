97a49f042eabd891fa5dfcc967ae804653f19dab
Demiantcev <kirushakotik@gmail.com>
2022-02-13 21:42:25 +0500

    corrected the text

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var birthLabel: UILabel!
+    @IBOutlet weak var fotoImage: UIImageView!
+    @IBOutlet weak var wordText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 }
