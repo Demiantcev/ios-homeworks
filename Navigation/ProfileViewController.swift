af659d11eb0245126a7bfced5cfbcd776a4b3f75
Demiantcev <kirushakotik@gmail.com>
2022-02-21 18:32:32 +0500

    finished homework

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -8,13 +8,8 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var cityLabel: UILabel!
-    @IBOutlet weak var birthLabel: UILabel!
-    @IBOutlet weak var fotoImage: UIImageView!
-    @IBOutlet weak var wordText: UITextView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        
     }
 }
